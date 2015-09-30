6224ee57e5235aeba58aee0c4427722be46fbfe1
Weicheng Pan <wcpan@yahoo-inc.com>
2015-09-30 11:05:48 +0800

    adjust put_content script to generate string value

--- demo/bin/put_content.sh
+++ demo/bin/put_content.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-if [ $# < 2 ]; then
+echo $#
+if [ $# -lt 2 ]; then
   echo "Set value to key via web service";
   echo "";
   echo "Usage: $0 key value [host1|host2|host3]";
   exit;
 fi
 
-if [ $3 = "host2" ]; then
+if [ "$3" = "host2" ]; then
   port=8081
-elif [ $3 = "host3" ]; then
+elif [ "$3" = "host3" ]; then
   port=8082
 else
   port=8080
 fi
-curl -X PUT -d "{\"key\":\"$1\",\"value\":\"$2\"}" -H "Content-Type: application/json" -v localhost:$port/api/entries/$1
+curl -X PUT -d "$2" -v localhost:$port/api/entries/$1
