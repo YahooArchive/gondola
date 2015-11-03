ea3a61b7c77a08c582bd9ac94c2588a4a2831e78
Weicheng Pan <weichengpan@gmail.com>
2015-11-02 22:38:28 -0800

    Add copyright header

--- containers/registration/src/test/java/com/yahoo/gondola/container/RegistryClientsTest.java
+++ containers/registration/src/test/java/com/yahoo/gondola/container/RegistryClientsTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2015, Yahoo Inc.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
 package com.yahoo.gondola.container;
 
 import com.yahoo.gondola.Config;
