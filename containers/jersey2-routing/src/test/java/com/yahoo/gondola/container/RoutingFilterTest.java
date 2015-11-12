4e69e46e249117cbe230ab9fdee7748da70e8433
Weicheng Pan <weichengpan@gmail.com>
2015-11-12 00:36:39 -0800

    Add Copyright

--- containers/jersey2-routing/src/test/java/com/yahoo/gondola/container/RoutingFilterTest.java
+++ containers/jersey2-routing/src/test/java/com/yahoo/gondola/container/RoutingFilterTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2015, Yahoo Inc.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+
 package com.yahoo.gondola.container;
 
 import com.yahoo.gondola.Cluster;
--- containers/registration/src/main/java/com/yahoo/gondola/container/RegistryClient.java
+++ containers/registration/src/main/java/com/yahoo/gondola/container/RegistryClient.java
@@ -3,6 +3,7 @@
  * Copyrights licensed under the New BSD License.
  * See the accompanying LICENSE file for terms.
  */
+
 package com.yahoo.gondola.container;
 
 import java.io.IOException;
