3e4baa88e69fca5dc1d69fc5c4c6f052dee201ac
Weicheng Pan <weichengpan@gmail.com>
2015-11-14 15:11:50 -0800

    improve running script

--- examples/kv-server/run.sh
+++ examples/kv-server/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 rm /tmp/gondola*
-/usr/bin/killall java
 ./bin/run host2 2>&1 > target/host2 &
+pid1=$!
 ./bin/run host3 2>&1 > target/host3 &
+pid2=$!
+trap "echo 'terminate running processes...'; kill $pid1 $pid2; wait" SIGINT SIGTERM
+wait
