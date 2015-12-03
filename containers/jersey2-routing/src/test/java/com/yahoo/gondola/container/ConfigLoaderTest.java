4ef93ef33a1b322ee4a0f45abbd721bf38dbdc51
Weicheng Pan <weichengpan@gmail.com>
2015-12-02 17:17:12 -0800

    rollback curator version. buggy in latest.

---
+++ containers/jersey2-routing/src/test/java/com/yahoo/gondola/container/ConfigLoaderTest.java
@@ -0,0 +1,10 @@
+package com.yahoo.gondola.container;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by wcpan2 on 12/2/15.
+ */
+public class ConfigLoaderTest {
+
+}
---
+++ containers/jersey2-routing/src/test/java/com/yahoo/gondola/container/ConfigWriterTest.java
@@ -0,0 +1,21 @@
+package com.yahoo.gondola.container;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by wcpan2 on 12/2/15.
+ */
+public class ConfigWriterTest {
+
+    @Test
+    public void testSave() throws Exception {
+
+    }
+
+    @Test
+    public void testSetBucketMap() throws Exception {
+
+    }
+}
